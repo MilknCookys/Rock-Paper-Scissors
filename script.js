d997c1c1737d08f36b7afb2ee29d5d98113b1ec6
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-05-10 11:04:34 +1000

    Completely moved all prior code to work with UI

--- script.js
+++ script.js
@@ -5,19 +5,139 @@ let playerScore = 0;
 // Generates signal by assigning a signal value to random number between 1-3
 function createComputerSignal() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  console.log(randomNumber);
+
   let signal;
 
   switch (randomNumber) {
     case 1:
       signal = "Rock";
-      return signal;
+      break;
 
     case 2:
       signal = "Paper";
-      return signal;
+      break;
 
     case 3:
       signal = "Scissors";
-      return signal;
+      break;
+  }
+
+  // console.log(signal);
+
+  return signal;
+}
+
+// function test() {
+//   console.log(computerSignal);
+// }
+
+function clearDisplay() {
+  const signalComparisonContainer = document.querySelector(
+    "#signalComparisonContainer"
+  );
+  signalComparisonContainer.textContent = "";
+}
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+let playerSignal = "";
+
+rock.addEventListener("click", () => {
+  playerSignal = "Rock";
+  console.log("Player chose Rock!");
+  clearDisplay();
+  computerChoice = createComputerSignal();
+  displayComputerSignal(computerChoice);
+  compareSignals(computerChoice, playerSignal);
+  changeScore();
+  winCheck();
+});
+
+paper.addEventListener("click", () => {
+  playerSignal = "Paper";
+  console.log("Player chose Paper!");
+  clearDisplay();
+  computerChoice = createComputerSignal();
+  displayComputerSignal(computerChoice);
+  compareSignals(computerChoice, playerSignal);
+  changeScore();
+  winCheck();
+});
+
+scissors.addEventListener("click", () => {
+  playerSignal = "Scissors";
+  console.log("Player chose Scissors!");
+  clearDisplay();
+  computerChoice = createComputerSignal();
+  displayComputerSignal(computerChoice);
+  compareSignals(computerChoice, playerSignal);
+  changeScore();
+  winCheck();
+});
+
+// This code displays the computer signal
+
+function displayComputerSignal(computerChoice) {
+  const computerSignal = document.createElement("p");
+  computerSignal.setAttribute("id", "computerSignal");
+
+  computerSignal.textContent = `Computer chose ${computerChoice}`;
+
+  const signalComparisonContainer = document.querySelector(
+    "#signalComparisonContainer"
+  );
+  signalComparisonContainer.appendChild(computerSignal);
+}
+
+function compareSignals(computerSignal, playerSignal) {
+  console.log(`computerSignal: ${computerSignal}`);
+  console.log(`playerSignal: ${playerSignal}`);
+
+  const winDisplay = document.createElement("p");
+  winDisplay.setAttribute("id", "winDisplay");
+
+  if (playerSignal === computerSignal) {
+    winDisplay.textContent = `Player chose ${playerSignal}
+     and computer chose ${computerSignal}. No points were won.`;
+  } else if (playerSignal === "Rock" && computerSignal === "Scissors") {
+    winDisplay.textContent = `${playerSignal} beats ${computerSignal}`;
+    playerScore++;
+  } else if (playerSignal === "Paper" && computerSignal === "Rock") {
+    winDisplay.textContent = `${playerSignal} beats ${computerSignal}`;
+    playerScore++;
+  } else if (playerSignal === "Scissors" && computerSignal === "Paper") {
+    winDisplay.textContent = `${playerSignal} beats ${computerSignal}`;
+    playerScore++;
+  } else {
+    winDisplay.textContent = `${computerSignal} beats ${playerSignal}`;
+    computerScore++;
+  }
+
+  const signalComparisonContainer = document.querySelector(
+    "#signalComparisonContainer"
+  );
+
+  signalComparisonContainer.appendChild(winDisplay);
+}
+
+function changeScore() {
+  const playerScoreElement = document.getElementById("playerScore");
+  const computerScoreElement = document.getElementById("computerScore");
+
+  console.log(playerScoreElement);
+
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+}
+
+function winCheck() {
+  if (playerScore === 5 || computerScore === 5) {
+    console.log("Stop");
+  } else {
+    console.log("Continue");
   }
 }
